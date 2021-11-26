db54e7745663485e627a2b13bee554fd63d8e3aa
LevaiViktoria <levaiviktoriaanna@gmail.com>
2021-11-26 07:48:31 +0100

    SoldierAPI

---
+++ src/main/java/hu/consumeSoldierAPI/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package hu.consumeSoldierAPI.controllers;
+
+import hu.consumeSoldierAPI.service.SoldierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private SoldierService service;
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/soldiers")
+    public String getSoldiers(Model model){
+        model.addAttribute("soldiers", service.getSoldiers());
+        return "soldiers";
+    }
+}
